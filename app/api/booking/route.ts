5af062f54befaacdcc7379e8ada2e54cc1240e5a
Nauflaganta <rindang.naufal2@gmail.com>
2026-03-11 22:43:09 +0700

    disable prisma for deploy

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -1,39 +1,18 @@
 export const dynamic = "force-dynamic";
 
-import { prisma } from "@/lib/prisma";
-
 export async function POST(req: Request) {
   try {
+
     const data = await req.json();
 
-    const booking = await prisma.bookings.create({
-      data: {
-        bookingId: "RINJANI-" + Date.now(),
-        fullname: data.fullname,
-        nationality: data.nationality,
-        phone: data.phone,
-        email: data.email,
-        service: data.service,
-        adults: Number(data.adults),
-        kids: Number(data.kids),
-        pickupDate: new Date(data.pickupDate),
-        pickupLocation: data.pickupLocation,
-        trekFrom: new Date(data.trekFrom),
-        trekTo: new Date(data.trekTo),
-        notes: data.notes,
-        paymentMethod: data.paymentMethod,
-        status: "pending"
-      }
-    });
+    console.log("Booking received:", data);
 
     return Response.json({
       success: true,
-      booking
+      message: "Booking berhasil diterima"
     });
 
   } catch (error) {
-
-    console.error(error);
 
     return Response.json({
       success: false,
